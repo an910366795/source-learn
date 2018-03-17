66e9a4804f96fd9099f650a35ea2d4ea8ddcedb7
chengan <an3188372@163.com>
2018-03-17 09:26:36 +0800

    项目初始化

---
+++ src/main/java/com/learning/jdk/util/MyHashMap.java
@@ -0,0 +1,82 @@
+package com.learning.jdk.util;
+
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author chengan.liang
+ * @deprecated:HashMap源码学习
+ * @since 2018-03-17 9:19
+ */
+
+
+public class MyHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>{
+
+
+
+
+
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
